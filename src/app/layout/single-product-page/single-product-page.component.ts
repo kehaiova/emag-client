6ce81b8dc6092c42c4276944931059e9f19281f2
Samir Dermendzhiev <samir_d@abv.bg>
2023-01-18 22:44:26 +0200

    Fix errors

--- src/app/layout/single-product-page/single-product-page.component.ts
+++ src/app/layout/single-product-page/single-product-page.component.ts
@@ -12,28 +12,26 @@ import { ProductService } from 'src/app/services/ProductService';
 })
 export class SingleProductPageComponent implements OnInit {
   sub: SubCategory = {
-    id: '1',
-    categoryId: '1',
-    name: 'test',
+    id: 1,
+    subcategoryName: 'test',
   };
 
-  tempProduct: Product=
-    {
-      id: 1,
-      name: 'test',
-      brand: 'test',
-      model: 'test',
-      price: 2,
-      subCategory: this.sub,
-      discountedPrice: 1,
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas consequat, urna a congue sodales, nunc mauris ultricies odio, a sodales ante erat blandit elit. Morbi consectetur dui at mi dictum dignissim. Nulla vel porta lectus, nec semper dolor. Mauris non urna accumsan, venenatis nulla eget, consectetur nisi. Donec velit lectus, ultricies ac semper eget, consectetur sed enim. Phasellus ultricies hendrerit placerat. Quisque viverra urna eleifend tempor ullamcorper. Quisque nec vulputate leo, at hendrerit sem. Vestibulum vulputate ipsum neque. Sed sit amet sapien sed odio aliquet accumsan in a est. Maecenas ultricies semper enim, eu faucibus ex finibus ut. Duis nulla libero, posuere et turpis non, sagittis blandit tellus. Integer laoreet, ex vel suscipit dignissim, urna augue porta enim, a placerat velit lectus ut dui.',
-      quantity: 10,
-      warrantyMonths: 12,
-      addedAt: new Date(),
-      deletedAt: new Date(),
-      productRating: 10,
-    };
+  tempProduct: Product = {
+    id: 1,
+    name: 'test',
+    brand: 'test',
+    model: 'test',
+    price: 2,
+    subCategory: this.sub,
+    discountedPrice: 1,
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas consequat, urna a congue sodales, nunc mauris ultricies odio, a sodales ante erat blandit elit. Morbi consectetur dui at mi dictum dignissim. Nulla vel porta lectus, nec semper dolor. Mauris non urna accumsan, venenatis nulla eget, consectetur nisi. Donec velit lectus, ultricies ac semper eget, consectetur sed enim. Phasellus ultricies hendrerit placerat. Quisque viverra urna eleifend tempor ullamcorper. Quisque nec vulputate leo, at hendrerit sem. Vestibulum vulputate ipsum neque. Sed sit amet sapien sed odio aliquet accumsan in a est. Maecenas ultricies semper enim, eu faucibus ex finibus ut. Duis nulla libero, posuere et turpis non, sagittis blandit tellus. Integer laoreet, ex vel suscipit dignissim, urna augue porta enim, a placerat velit lectus ut dui.',
+    quantity: 10,
+    warrantyMonths: 12,
+    addedAt: new Date(),
+    deletedAt: new Date(),
+    productRating: 10,
+  };
 
   product: Observable<Product> = EMPTY;
 
